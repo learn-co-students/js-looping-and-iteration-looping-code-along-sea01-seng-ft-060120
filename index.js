d5797effa16b78a99850490fec3b27a6b9b0507d
jlorenc1 <joshua.lorenc@icloud.com>
2020-07-14 10:21:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let thanks = []
+    for (let i = 0;  i < array.length; i++) {
+        thanks.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`); 
+    }
+    return thanks;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number--
+    }
+    console.log(number)
+}
+
